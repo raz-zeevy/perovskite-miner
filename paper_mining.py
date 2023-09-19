7a2b8dda15654944b34b83585dbd6a25c6bab825
Raz Zeevy <raz3zeevy@gmail.com>
2023-09-19 15:32:03 +0200

    added logging and progress with pipeline

--- paper_mining.py
+++ paper_mining.py
@@ -1,17 +1,46 @@
-from typing import List, Tuple
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
-from questions_const import FIELD_NAME, PROTOCOL_QUESTION, QID, GPT_QUESTION
-from gpt_api import access_chat_gpt_3
+from gpt_api import post_paper_prompt
+from prompt_engineering.prompt_engineering import PaperPrompt
+import datetime
+from data_exploration.utlis import *
 
+LOG_ROW_LENGTH = 80
 
-def generate_prompts(pdf_path: str, questions: List[str]) -> List[str]: pass
+def log_gpt_results(p_prompt: PaperPrompt, res, pdf_path,
+                    accuracy, fields) -> None:
+    def log_path(paper_pdf_path):
+        split_name = paper_pdf_path.split(".")
+        output = ".".join(split_name[:-1])
+        return output + "_res.log"
 
+    pdf_name = pdf_path.split("\\")[-1].split(".")[0]
+    log = f"Date: [{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]\n"
+    log += f"Paper: {pdf_name}\n"
+    log += f"API Calls: {p_prompt.number_of_api_calls}\n"
+    log += f"Shrinking methods: {p_prompt.shrink_method}\n"
+    log += f"Questions count: {len(p_prompt.questions)}\n"
+    log += f"Questions per API call: {p_prompt.questions_per_api_call}\n"
+    log += f"Tokens per API call: {p_prompt.tokens_per_api_call}\n"
+    log += f"Prompt Length: {p_prompt.paper_prompt_tokens} tokens\n"
+    log += f"Response Accuracy: {accuracy:.2f}%\n"
+    log += "-" * LOG_ROW_LENGTH + "\n"
+    log += f"Prompt: \n{p_prompt.contents[0]}\n"
+    log += "-" * LOG_ROW_LENGTH + "\n"
+    log += f"Response: \n{res}\n"
+    log += "-" * LOG_ROW_LENGTH + "\n"
+    log += f"Fields: {', '.join(fields)}\n"
+    log += ("=" * LOG_ROW_LENGTH + "\n") * 2
 
-def gpt_fill(paper_pdf_path, fields=None, questions=None, qids=None) -> \
+    with open(log_path(paper_pdf_path), "a", encoding='utf-8') as output:
+        output.write(log)
+
+def gpt_fill(paper_pdf_path, fields=None, questions=None, qids=None,
+             fake=False) -> \
         DataFrame:
-    questions_db_path = r"data/questions/questions_db.csv"
-    q_df = pd.read_csv(questions_db_path)
+    # todo write in people who insert the model name and remove this question
+    q_df = load_questions_db()
     # filter data according to given filters
     q_df = q_df[q_df[FIELD_NAME].notna()]
     for filter, values in {FIELD_NAME: fields,
@@ -19,19 +48,33 @@ def gpt_fill(paper_pdf_path, fields=None, questions=None, qids=None) -> \
                            QID: qids}.items():
         if values:
             q_df = q_df[q_df[filter].isin(values)]
-    # TODO: make the psuedo code work
-    # prompts = generate_prompts(paper_pdf_path, q_df[GPT_QUESTION].values)
-    p_prompts = PaperPrompt(paper_pdf_path, q_df[GPT_QUESTION].values)
-    for i in range(p_prompts.num_of_api_calls):
-        res = access_chat_gpt_3(p_prompts.contents[i])
-    # Structure the results
-    res = res.split("\n")
-    res_df = pd.DataFrame(columns=q_df[FIELD_NAME].values)
-    res_df.append(res, ignore_index=True)
-    return res_df
+    p_prompts = PaperPrompt(paper_pdf_path, q_df[GPT_QUESTION].values,
+                            int(1.6e4))
+    res = post_paper_prompt(p_prompts, fake=fake)
+    log_gpt_results(p_prompts, res, paper_pdf_path, 0, q_df[FIELD_NAME].
+                    values)
+    # check if results are unsuccessful
+    if ":" not in res:
+        return res
+    return results_to_df(res, columns=q_df[FIELD_NAME])
 
+def results_to_df(res, columns):
+    api_cols = [value.split(":")[0] for value in res.split("\n")]
+    values = [value.split(":")[1] for value in res.split("\n")]
+    res_df = pd.DataFrame(columns=columns)
+    res_df.append(api_cols, ignore_index=True)
+    res_df.append(values, ignore_index=True)
+    return res_df
+def mine_paper(paper_pdf_path):
+    def output_name(paper_pdf_path):
+        split_name = paper_pdf_path.split(".")
+        output = ".".join(split_name[:-1])
+        return output + "_api_results.csv"
+
+    y_pred = gpt_fill(paper_pdf_path, fake=True)
+    if isinstance(y_pred, DataFrame):
+        y_pred.to_csv(output_name(paper_pdf_path))
 
 if __name__ == '__main__':
-    paper_pdf_path = r"data/paper1.pdf"
-    paper_data_path = r"data/db_output1.pdf"
-    y_pred = gpt_fill(paper_pdf_path)
+    paper_pdf_path = r"data/papers/downloads/10.1002_adem.201900288.pdf"
+    y_pred = mine_paper(paper_pdf_path)
